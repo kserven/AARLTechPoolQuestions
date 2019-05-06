ee7caf25a3c36e334050cf3e7a23efb43d59ff7e
Keith Serven <kserven@gmail.com>
2019-05-05 18:33:48 -0700

    Add project files.

---
+++ AARLTechPool/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace AARLTechPool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var questions = new List<Question>();
+
+            var question = new Question
+            {
+                Id = "T1A01",
+                Answer = "C",
+                Meta = "97.1",
+                QuestionText =
+                    "Which of the following is a purpose of the Amateur Radio Service as stated in the FCC rules and regulations?",
+                Choices =
+                {
+                    [0] = "A. Providing personal radio communications for as many citizens as possible",
+                    [1] = "B. Providing communications for international non-profit organizations",
+                    [2] = "C. Advancing skills in the technical and communication phases of the radio art",
+                    [3] = "D. All of these choices are correct"
+                }
+            };
+
+            questions.Add(question);
+            questions.Add(question);
+
+        }
+    }
+}
---
+++ AARLTechPool/Question.cs
@@ -0,0 +1,22 @@
+﻿namespace AARLTechPool
+{
+    internal class Question
+    {
+        public string Id { get; set; }
+        public string Answer { get; set; }
+        public string Meta { get; set; }
+        public string QuestionText { get; set; }
+        public string[] Choices { get; set; }
+
+        public Question() { }
+        public Question(string id, string answer, string meta, string question, string[] choices)
+        {
+            Id = id;
+            Answer = answer;
+            Meta = meta;
+            QuestionText = question;
+            Choices = choices;
+        }
+
+    }
+}
